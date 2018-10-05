3ca04cda33e6c88813d832ab6b28431cc91b2889
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2018-10-05 14:25:42 +0100

    Inital

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+ * This is index.js
+ * Start by modifying the id, fn and sn functions to return
+ * information about you, then open index.html to check what
+ * else you have to do, adding functions to the end of this
+ * file as necessary.
+ *
+ * NB: all code you write this year shoudl use strict mode, so
+ * we've enabled that by default with the first line of code.
+ */
+
+'use strict';
+
+function id() {
+    return "UP879064";
+    // e.g. return "UP654321";
+}
+
+function fn() {
+    return "Finlay";
+}
+
+function sn() {
+    return "Plant";
+}
+
+// add your functions here
