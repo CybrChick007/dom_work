c77ccdd86d8952515310af2e210e76ff36492802
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2018-10-11 08:59:17 +0100

    replaceText/AddTextTo

--- index.js
+++ index.js
@@ -12,16 +12,30 @@
 'use strict';
 
 function id() {
-    return "UP879064";
-    // e.g. return "UP654321";
+  return "UP879064";
+  // e.g. return "UP654321";
 }
 
 function fn() {
-    return "Finlay";
+  return "Finlay";
 }
 
 function sn() {
-    return "Plant";
+  return "Plant";
 }
 
 // add your functions here
+
+function replaceText(element, string) {
+  element.textContent = string;
+  return element;
+}
+
+function addTextTo(addto, newText) {
+  window.addto.textContent = addto.textContent + newText;
+  return addto, newText;
+}
+
+function moreBears() {
+
+}
